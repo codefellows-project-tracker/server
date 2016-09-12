0ce76e3cb8554de31adcb7300902191f520ddd20
preetsohal <preetsohal@live.com>
2016-09-12 16:40:12 -0700

    created post and getOne user routes

--- src/routes/user.js
+++ src/routes/user.js
@@ -13,5 +13,17 @@ router.get('/', (req, res) => {
       res.status(500).json({ message: 'ERROR!!!' });
     });
 });
-
+router.get('/:id', (req, res) => {
+  User.findOne({ _id: req.params.id })
+  .then((user) => {
+    res.json(user);
+  });
+});
+router.post('/', (req, res) => {
+  const user = new User(req.body);
+  user.save()
+  .then((newUser) => {
+    res.json(newUser);
+  });
+});
 module.exports = router;
