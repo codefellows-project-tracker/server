6dfb5a0d87e6c84fe79cc080eec1a069b1f43088
Gio d'Amelio <giodamelio@gmail.com>
2016-09-12 22:20:08 -0700

    Add debug module for debug logging

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const debug = require('debug')('cpt:router/user');
 
 const User = require('../models/user');
 
@@ -9,36 +10,55 @@ router.get('/', (req, res) => {
     .then((users) => {
       res.json(users);
     })
-    .catch(() => {
+    .catch((err) => {
+      debug(err);
       res.status(500).json({ message: 'ERROR!!!' });
     });
 });
+
 router.get('/:id', (req, res) => {
   User.findOne({ _id: req.params.id })
-  .then((user) => {
-    res.json(user);
-  });
+    .then((user) => {
+      res.json(user);
+    })
+    .catch((err) => {
+      debug(err);
+      res.status(500).json({ message: 'ERROR!!!' });
+    });
 });
+
 router.post('/', (req, res) => {
   const user = new User(req.body);
   user.save()
-  .then((newUser) => {
-    res.json(newUser);
-  });
+    .then((newUser) => {
+      res.json(newUser);
+    })
+    .catch((err) => {
+      debug(err);
+      res.status(500).json({ message: 'ERROR!!!' });
+    });
 });
+
 router.put('/:id', (req, res) => {
   User.findOneAndUpdate({ _id: req.params.id }, req.body, { new: true })
-  .then((updatedUser) => {
-    res.json(updatedUser);
-  })
-  .catch(() => {
-    res.status(500).json({ message: 'ERROR!!!' });
-  });
+    .then((updatedUser) => {
+      res.json(updatedUser);
+    })
+    .catch((err) => {
+      debug(err);
+      res.status(500).json({ message: 'ERROR!!!' });
+    });
 });
+
 router.delete('/:id', (req, res) => {
   User.remove({ _id: req.params.id })
-  .then((deleteUser) => {
-    res.json(deleteUser);
-  });
+    .then((deleteUser) => {
+      res.json(deleteUser);
+    })
+    .catch((err) => {
+      debug(err);
+      res.status(500).json({ message: 'ERROR!!!' });
+    });
 });
+
 module.exports = router;
