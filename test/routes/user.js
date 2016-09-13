4bc81d4bda66eb3581b590b594956a7aa227515d
Gio d'Amelio <giodamelio@gmail.com>
2016-09-13 09:59:22 -0700

    Test PUT and DELETE

--- test/routes/user.js
+++ test/routes/user.js
@@ -85,4 +85,65 @@ describe('/api/user', () => {
         })
     ));
   });
+
+  describe('PUT', () => {
+    it('should 404 on invalid user', function() {
+      return supertest(server)
+        .put(`/api/user/${this.id}`)
+	.send({ name: 'HAHA' })
+        .expect(200)
+        .then((res) => {
+          expect(res.body.name).to.equal('HAHA');
+        });
+    });
+
+    it('should 404 on invalid user', () => (
+      supertest(server)
+        .put('/api/user/57d6e7c2f532ad68ac3d9424')
+	.send({ name: 'HAHA' })
+        .expect(404)
+        .then((res) => {
+          expect(res.body.message).to.equal('User not found');
+        })
+    ));
+
+    it('should 404 on invalid user id', () => (
+      supertest(server)
+        .put('/api/user/haha')
+        .expect(404)
+        .then((res) => {
+          expect(res.body.message).to.contain('Cast to ObjectId');
+        })
+    ));
+  });
+
+  describe('DELETE', () => {
+    it('should 404 on invalid user', function() {
+      return supertest(server)
+        .delete(`/api/user/${this.id}`)
+        .expect(200)
+        .then((res) => {
+          expect(res.body.ok).to.equal(1);
+          expect(res.body.n).to.equal(1);
+        });
+    });
+
+    it('should 404 on invalid user', () => (
+      supertest(server)
+        .delete('/api/user/57d6e7c2f532ad68ac3d9424')
+        .expect(404)
+        .then((res) => {
+          expect(res.body.message).to.equal('User not found');
+        })
+    ));
+
+    it('should 404 on invalid user id', () => (
+      supertest(server)
+        .delete('/api/user/haha')
+        .expect(404)
+        .then((res) => {
+          expect(res.body.message).to.contain('Cast to ObjectId');
+        })
+    ));
+  });
 });
