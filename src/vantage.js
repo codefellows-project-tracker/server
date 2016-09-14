b282df2fe72da762a41022fee73d627378d0b211
Gio d'Amelio <giodamelio@gmail.com>
2016-09-13 23:24:35 -0700

    Validate enviroment variables with envalid. Setup vantage auth

--- src/vantage.js
+++ src/vantage.js
@@ -2,9 +2,23 @@ const Vantage = require('vantage');
 const Table = require('cli-table');
 const chalk = require('chalk');
 
+const config = require('./config');
 const User = require('./models/user');
 
 const vantage = new Vantage();
+
+vantage.auth('basic', {
+  users: [
+    {
+      user: config.VANTAGE_USERNAME,
+      pass: config.VANTAGE_PASSWORD,
+    },
+  ],
+  retry: 3,
+  retryTime: 500,
+  deny: 1,
+  unlockTime: 3000,
+});
 
 // List all users
 vantage
