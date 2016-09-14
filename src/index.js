5576f4d0d3f9b48be9831e116cce36ea2b8f7f5c
Gio d'Amelio <giodamelio@gmail.com>
2016-09-14 09:13:26 -0700

    Lint

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ mongoose.Promise = Promise;
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/cpt');
 
 const PORT = process.env.PORT || 3141;
-vantage.listen(app, PORT, function() {
+vantage.listen(app, PORT, () => {
   console.log('Vantage connected'); // eslint-disable-line no-console
 });
 console.log(`Server listening at http://localhost:${PORT}`); // eslint-disable-line no-console
