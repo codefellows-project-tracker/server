0438bc3ee5233091161dc18e9449954f4ccdcbcb
jp <gaines.johnp@gmail.com>
2016-09-14 13:18:07 -0700

    removed auth from user route

--- src/routes/user.js
+++ src/routes/user.js
@@ -39,7 +39,7 @@ router.get('/:id', (req, res) => {
     });
 });
 
-router.post('/', IsAuthenticated, (req, res) => {
+router.post('/', (req, res) => {
   const user = new User(req.body);
   user.save()
     .then((newUser) => {
