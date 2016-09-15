bd37685adcf9d8ddb5172e853d5ce0c0680dea74
jp <gaines.johnp@gmail.com>
2016-09-14 18:34:27 -0700

    added basic role system

--- src/authMiddleware.js
+++ src/authMiddleware.js
@@ -4,19 +4,27 @@ const jwt = require('jsonwebtoken');
 const config = require('./config');
 const errorHelper = require('./errorHelper');
 
-module.exports = function IsAuthenticated(req, res, next) {
-  const header = req.get('authorization');
-  if (!header) {
-    return errorHelper(res, 401)(new Error('Not Authorization header'));
-  }
-
-  const token = authHeader.parse(header).token;
-  jwt.verify(token, config.SECRET, (err, tokenData) => {
-    if (err) {
-      errorHelper(res, 401)(new Error('Not authorized'));
-    } else {
-      req.user = tokenData;
-      next();
+module.exports = function(...roles) {
+  return function IsAuthenticated(req, res, next) {
+    const header = req.get('authorization');
+    if (!header) {
+      return errorHelper(res, 401)(new Error('Not Authorization header'));
     }
-  });
+
+    const token = authHeader.parse(header).token;
+    jwt.verify(token, config.SECRET, (err, tokenData) => {
+      if (err) {
+        errorHelper(res, 401)(new Error('Not authorized'));
+      } else {
+        if (tokenData.role === 'admin') {
+          req.user = tokenData;
+          return next();
+        } else if (roles.includes(tokenData.role)) {
+          req.user = tokenData;
+          return next();
+        }
+        return errorHelper(res, 401)(new Error('Not authorized'));
+      }
+    });
+  };
 };
